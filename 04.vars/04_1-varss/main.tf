f151b31abfcfe2faa2d02e83cb84389ba222fbad
Murthy <murthychiluka@gmail.com>
2023-02-25 16:26:41 +0530

    ind

--- 04.vars/04_1-varss/main.tf
+++ 04.vars/04_1-varss/main.tf
@@ -1,17 +1,16 @@
-variable "dadi" {
+variable "d1" {
   default = [
-   {
-     course_name = "aws"
-     trainer_name = "sudeep"
-   },
-   { 
-    course_name = "devops"
-    trainer_name" ="Anudeep"
-   }
-
+    {
+      course_name  = "aws"
+      trainer_name = "raju"
+    },
+    {
+      course_name  = "devops"
+      trainer_name = "ravi"
+    }
   ]
 }
 
-output "course_name" {
-  value = var.dadi.*.course_name
+output "course_names" {
+  value = var.d1.*.course_name
 }
