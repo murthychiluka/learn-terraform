b04be6825c21f0732f5f584acc23ac2232cfc502
Murthy <murthychiluka@gmail.com>
2023-02-25 14:39:51 +0530

    c

--- roboshop-setup/example2/main.tf
+++ roboshop-setup/example2/main.tf
@@ -6,14 +6,14 @@ data "aws_ami" "ami" {
 
 
 resource "aws_instance" "frontend" {
-    count = 5
-  ami                    = data.aws_ami.ami.image_id
-  instance_type          = "t3.micro"
+    count                = length("var.instances")
+    ami                  = data.aws_ami.ami.image_id
+    instance_type        = "t2.micro"
   vpc_security_group_ids = ["sg-0e05adc3a9fdbddf0"]
  
 }
 
 variable "instances"  {
-  default ["frontend, "mongodb", "user", "catalogue","cart" ]  
+  default = ["frontend , "mongodb" , "user" , "catalogue" , "cart" ]  
 
 }
