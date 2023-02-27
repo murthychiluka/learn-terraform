118accc50cb741e7e928f751c2d21ece39520d5d
Murthy <murthychiluka@gmail.com>
2023-02-27 14:39:00 +0530

    m

--- roboshop-setup/example4/ec2/main.tf
+++ roboshop-setup/example4/ec2/main.tf
@@ -15,12 +15,13 @@ resource "aws_instance" "ec2" {
 }
 
 resource "aws_route53_record" "record" {
+
   zone_id = "Z08295911TJBO1YG96BEC"
   name    = "${var.component}-dev.murthychiluka.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
-
+}
 
 variable "component" {}
 variable "instance_type" {}
