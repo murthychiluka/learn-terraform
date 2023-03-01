eb9620e6fe8fe9df67146e3c5bc82ac1d2bb6b50
Murthy <murthychiluka@gmail.com>
2023-03-01 11:53:48 +0530

    d

--- 10-provisioners/main.tf
+++ 10-provisioners/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
   ami                    = "ami-0a017d8ceb274537d"
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = ["sg-0e05adc3a9fdbddf0"]
   tags = {
     Name = "demo"
