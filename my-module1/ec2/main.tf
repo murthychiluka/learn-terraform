ed4f3ffb2b8bb05d0354569c67282b03ef9c434a
Murthy <murthychiluka@gmail.com>
2023-03-09 14:04:47 +0530

    d

--- my-module1/ec2/main.tf
+++ my-module1/ec2/main.tf
@@ -1,6 +1,8 @@
-variable "instance_type" {}
+
 
 resource "aws_instance" "my-instance" {
   ami           = "ami-03fb284a84828c77a"
   instance_type = var.instance_type
 }
+
+variable "instance_type" {}
