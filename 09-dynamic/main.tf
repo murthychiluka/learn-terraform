bbc51bcbd2f66938d60c001b8b7ff30bb3df04b9
Murthy <murthychiluka@gmail.com>
2023-03-28 15:00:37 +0530

    d

--- 09-dynamic/main.tf
+++ 09-dynamic/main.tf
@@ -1,26 +1,29 @@
+# #Using for_each function**
+# variable "bucket_names" {
+#   type    = list(string)
+#   default = ["sudeep", "aadvik", "ruchi"]
+# }
+
+# resource "aws_s3_bucket" "buckets" {
+#   #region   = "us-east-1"
+#   for_each = { for name in var.bucket_names : name => name }
+
+
+#   # acl = "private"
+# }
+
+
+#Using count function**
+
 variable "bucket_names" {
   type    = list(string)
-  default = ["sudeep", "aadvik", "ruchi"]
+  default = ["bucket1", "bucket2", "bucket3"]
 }
 
 resource "aws_s3_bucket" "buckets" {
-  #region   = "us-east-1"
-  for_each = { for name in var.bucket_names : name => name }
-
-
-  # acl = "private"
+  count = length(var.bucket_names)
+  #region  = "us-east-1"
+  bucket = var.bucket_names[count.index]
+  #acl     = "private"
 }
-
-
-# resource "aws_s3_bucket" "buckets" {
-
-
-#   for_each = var.bucket_names
-#
-
-#   bucket = each.value["bucket_names"]
-
-
-# }
-
 
